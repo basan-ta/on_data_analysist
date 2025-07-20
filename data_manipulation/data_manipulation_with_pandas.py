7904b5818951ae64e4023e418a1b9dbf947f5b77
Basantashrestha <basantashrestha@Basantas.local>
2025-07-20 10:55:18 +0545

    Added subsetting with index

--- data_manipulation/data_manipulation_with_pandas.py
+++ data_manipulation/data_manipulation_with_pandas.py
@@ -7,6 +7,19 @@ data = Path('data') / 'employee_data.csv'
 # Read the CSV into a DataFrame
 df = pd.read_csv(data)
 
-# Print the DataFrame
-print(df.head())
-print(df.describe())
+#print dataset 
+#print(df)
+
+#Subsetting with index 
+#1 Set the index of employee_data to name 
+name_index = df.set_index('name')
+
+
+# Look at df
+print(name_index)
+
+# Reset the df index, keeping its contents
+print(name_index.reset_index())
+
+# Reset the df index, dropping its contents
+print(name_index.reset_index(drop=True))
