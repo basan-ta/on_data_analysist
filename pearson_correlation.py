a774115877d6b6943704abbea743517d444934b9
Basantashrestha <basantashrestha@Basantas.local>
2025-08-04 16:17:27 +0545

    The final cor function has been created

--- pearson_correlation.py
+++ pearson_correlation.py
@@ -1,19 +1,22 @@
-def mean(x,y):
-#Formula to calculate Pearson correlation coefficient:
-#cov(x,y) / (std(x) * std(y))
-#cov(x,y) = sun((x- x.mean) * (y - y.mean)) / n
-#std(x) = ( sum((x-x.mean) **2) /n ) ** 0.5
-#std(y) = (sum((y-y.mean) **2) / N )n ** 0.5
+def pearson_cor(x,y):
     n = len(x)
     
     #calculate mean
     mean_x = sum(x)/n 
     mean_y = sum(y)/n
-    return mean_x, mean_y
+    #calculate std
+    std_x = (sum((i-mean_x) ** 2 for i in x) / n ) ** 0.5
+    std_y = (sum((i-mean_y) ** 2 for i in y) / n ) ** 0.5
+    #calculate covariance 
+    cov = sum((i - mean_x) * (j - mean_y) for i, j in zip(x,y)) / n
+    
+    # calculate Pearson correlation coefficient
+    cor = cov / (std_x * std_y)
+    return  cor
 
 x = [1,3,5,7,9,11]
-y = [2,4,6,8,10,12]
+y = [2,4,6,8,10,15]
 
-result = mean(x, y)
-print(f"Maan of x and y are : ", result)
+result = pearson_cor(x, y)
+print(f"Pearson correlation is ", result)
 
