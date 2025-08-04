ce7c35107a1964b385428815601656876df3e601
Basantashrestha <basantashrestha@Basantas.local>
2025-08-04 15:57:16 +0545

    Formula to calculate the cor

--- pearson_correlation.py
+++ pearson_correlation.py
@@ -6,3 +6,10 @@ y = [2,4,6,8,10,12]
 
 result = calculate_pearson(x, y)
 print(f"X and y are :", result)
+
+#Formula to calculate Pearson correlation coefficient:
+#cov(x,y) / (std(x) * std(y))
+#cov(x,y) = sun((x- x.mean) * (y - y.mean)) / n
+#std(x) = ( sum((x-x.mean) **2) /n ) ** 0.5
+#std(y) = (sum((y-y.mean) **2) / N )n ** 0.5
+
